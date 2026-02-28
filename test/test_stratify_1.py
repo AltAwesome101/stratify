18be45de93971fc447b43be67e3661a104449e35
Warren Gounden <warrenthomasgounden@gmail.com>
2026-02-28 22:57:26 +0200

    Added unit tests and coverage analysis

--- test/test_stratify_1.py
+++ test/test_stratify_1.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-
 import unittest
-
 import stratify
-
 
 class TestExclude(unittest.TestCase):
 
@@ -15,9 +12,32 @@ class TestExclude(unittest.TestCase):
     def test_centre2(self):
         evecs=[[0,0,0],[-1,0,1],[1,0,1],[0,4,0]]
         c1 = stratify.getCentroid(evecs,0,[0,1])
-        self.assertEqual(c1,1)
+        self.assertEqual(c1,-0.5)
+
+    def test_stddev1(self):
+        evecs = [[2,0,0],[2,0,0],[2,0,0],[2,0,0]]
+        result = stratify.getSampleStdDev(evecs, 0, [0,1,2,3], 2)
+        self.assertEqual(result, 0)
+
+    def test_stddev2(self):
+        evecs = [[2,0,0],[4,0,0],[4,0,0],[4,0,0],[5,0,0],
+                 [5,0,0],[7,0,0],[9,0,0]]
+        ave = stratify.getCentroid(evecs, 0, [0,1,2,3,4,5,6,7])
+        result = stratify.getSampleStdDev(evecs, 0, [0,1,2,3,4,5,6,7], ave)
+        self.assertAlmostEqual(result, 2.138, places=2)
+
+    def test_no_outliers(self):
+        evecs = [[1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0]]
+        result = stratify.remOutliers(evecs, ['a','b','c','d','e'], [0,1,2,3,4])
+        self.assertEqual(result, [])
+
+    def test_single_person(self):
+        evecs = [[1,0,0]]
+        result = stratify.remOutliers(evecs, ['a'], [0])
+        self.assertEqual(result, [])
+
+    def test_dummy(self):
+        stratify.dummy(5)
 
 if __name__ == '__main__':
-
-
     unittest.main()
--- test/test_stratify_2.py
+++ test/test_stratify_2.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python3
-
 import unittest
 import os
 import stratify
 
 this_dir = os.path.dirname(__file__)
 
-
 class TestExclude(unittest.TestCase):
 
     def setUp(self):
         self.test_data = stratify.inputData(os.path.join(this_dir,"sample.evec"))
-
+        self.evecs = self.test_data[0]
+        self.pop   = self.test_data[1]
+        self.pids  = self.test_data[2]
 
     def test_centre1(self):
-        c1 = stratify.getCentroid(self.test_data[0],1,self.test_data[1]["KS3"])
-        self.assertAlmostEqual(c1,-0.014050000000)
+        c1 = stratify.getCentroid(self.evecs, 1, self.pop["KS3"])
+        self.assertAlmostEqual(c1, -0.014050000000)
 
+    def test_populations_loaded(self):
+        # check that expected populations exist in the data
+        self.assertIn("Baganda_quad", self.pop)
+        self.assertIn("YRI", self.pop)
+        self.assertIn("CEU", self.pop)
 
+    def test_person_count(self):
+        # check total number of people loaded
+        self.assertEqual(len(self.evecs), len(self.pids))
+
+    def test_baganda_outlier(self):
+        # from running the program we know person index 17
+        # is an outlier in Baganda_quad
+        excludes = stratify.remOutliers(self.evecs, self.pids,
+                                        self.pop["Baganda_quad"])
+        self.assertIn(17, excludes)
+
+    def test_yri_outliers(self):
+        # YRI population should have outliers at 564 and 602
+        excludes = stratify.remOutliers(self.evecs, self.pids,
+                                        self.pop["YRI"])
+        self.assertIn(564, excludes)
+        self.assertIn(602, excludes)
+
+    def test_zulu_octo_no_outliers(self):
+        # Zulu_octo should have no outliers
+        excludes = stratify.remOutliers(self.evecs, self.pids,
+                                        self.pop["Zulu_octo"])
+        self.assertEqual(excludes, [])
 
 if __name__ == '__main__':
-
-
     unittest.main()
